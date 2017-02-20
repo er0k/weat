cfa9607c6e20cab493f2f94ff74ce2777948a075
er0k <er0k@er0k.net>
2017-02-20 17:01:21 -0500

    use service consts

--- src/Weat/WeatherService.php
+++ src/Weat/WeatherService.php
@@ -4,13 +4,19 @@ namespace Weat;
 
 class WeatherService
 {
+    const WEATHER_UNDERGROUND = 1;
+    const ACCUWEATHER = 2;
+    const OPEN_WEATHER_MAP = 3;
+    const NOAA = 4;
+    const FORECAST_IO = 5;
+
     private $service;
 
     /**
      * @param Config $config
-     * @param string $service
+     * @param int $service
      */
-    public function __construct(Config $config, $service = '')
+    public function __construct(Config $config, $service = self::WEATHER_UNDERGROUND)
     {
         $this->service = $this->getService($config, $service);
     }
@@ -24,18 +30,23 @@ class WeatherService
         return $this->service->getWeather($location);
     }
 
+    /**
+     * @param  Config $config
+     * @param  int $service
+     * @return AbstractWeatherService
+     */
     private function getService(Config $config, $service)
     {
         switch ($service) {
-            case 'wunderground':
+            case self::WEATHER_UNDERGROUND:
                 return new WeatherService\WeatherUnderground($config);
-            case 'accuweather':
+            case self::ACCUWEATHER:
                 return new WeatherService\AccuWeather($config);
-            case 'openweathermap':
+            case self::OPEN_WEATHER_MAP:
                 return new WeatherService\OpenWeatherMap($config);
-            case 'noaa':
+            case self::NOAA:
                 return new WeatherService\NOAA($config);
-            case 'forecastio':
+            case self::FORECAST_IO:
                 return new WeatherService\ForecastIO($config);;
             default:
                 throw new Exception("uknown weather service");
