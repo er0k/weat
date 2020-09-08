fff26755b6c1c0696060fdcc45ba2474a001a836
er0k <er0k@er0k.net>
2020-09-07 21:06:36 -0400

    boo docker logs. yay syslog

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,14 @@ docker stop weat
 docker rm weat
 docker build -t weat . && \
   docker create --name weat \
+    --log-driver syslog \
+    --log-opt mode=non-blocking \
+    --log-opt tag=docker-weat \
+    --log-opt syslog-address=udp://localhost:514 \
+    --log-opt syslog-facility=daemon \
     -p 6666:80 \
     -v /opt/geoip:/opt/geoip \
     -v /home/er0k/dev/weat:/var/www \
     --restart unless-stopped \
     weat && \
-docker start weat && \
-docker logs -f weat
+docker start weat
