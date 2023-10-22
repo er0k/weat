a58c32f7b0d445d12bdd0eb1bf8a24b189a7914a
er0k <er0k@er0k.net>
2023-10-22 19:02:47 -0400

    satisfy the type checker

--- src/Weat/Config.php
+++ src/Weat/Config.php
@@ -6,6 +6,13 @@ class Config
 {
     const CONFIG_FILE = 'config/config.php';
 
+    public string $open_weather_map_key;
+    public string $weat_db;
+    public string $city_db;
+    public string $station_type;
+    public string $station_id;
+    public array $default_location;
+
     public function __construct()
     {
         if (!$config = include self::CONFIG_FILE) {
